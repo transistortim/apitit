e6113cddb89426a4126f8b8630b4f61d42876534
Tim Hetkämper <transistortim@hetkaemper.it>
2026-05-16 22:59:51 +0200

    Add test for get_texts

--- tests/test_apitit.py
+++ tests/test_apitit.py
@@ -18,9 +18,28 @@ def apt(request):
 
 
 def test_credentials(apt):
-    """Test for meaningful credentials by calculating auth header."""
+    """Test for meaningful credentials by calculating auth header.
+
+    Internally, Apitit sends a request to the homepage URL. Thus the test also
+    indicates if the homepage URL is reachable.
+    """
     header_calc = f"{apt.api_credentials.user}:{apt.api_credentials.password}"
     header_calc = base64.b64encode(bytes(header_calc, 'utf-8'))
     header_calc = header_calc.decode('utf-8')
     header_calc = f"Basic {header_calc}"
     assert header_calc == apt.api_credentials.header
+
+
+def test_get_texts(apt):
+    """Test return value of get_texts.
+
+    get_texts should return a list of dictionaries with each dict containing
+    the keys "id" and "text".
+    Internally, Apitit sends a request to the kasvc URL. Thus the test also
+    indicates if the kasvc URL is reachable.
+    """
+    res = apt.get_texts()
+    assert isinstance(res, list)
+    assert isinstance(res[0], dict)
+    assert "id" in res[0]
+    assert "text" in res[0]
